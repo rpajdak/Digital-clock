ddccc132ca7cb86d181304ceef72def3863f4d46
rpajdak <30751691+rpajdak@users.noreply.github.com>
2018-11-22 23:52:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,15 +9,13 @@ function setDate() {
     const minutesseconddigit = document.querySelector("#digit4");
     const secondsfirstdigit = document.querySelector("#digit5");
     const secondsseconddigit = document.querySelector("#digit6");
-    console.log(seconds[1]);
-    for (i = 0; i < 10; i++) {
         hoursfirstdigit.className = ("digit num" + hours[0]);
         hoursseconddigit.className = ("digit num" + hours[1]);
         minutesfirstdigit.className = ("digit num" + minutes[0]);
         minutesseconddigit.className = ("digit num" + minutes[1]);
         secondsfirstdigit.className = ("digit num" + seconds[0]);
         secondsseconddigit.className = ("digit num" + seconds[1]);
-    }
+
 
 
 }
